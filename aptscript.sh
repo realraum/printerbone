60b8042cc682cb4e08d563d47b5529be5d2eed68
Bernhard Tittelbach <bernhard@tittelbach.org>
2020-01-19 05:30:19 +0100

    use inotify instead of polling

--- aptscript.sh
+++ aptscript.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get purge --yes --allow-change-held-packages bonescript apache2-bin apache2-data apache2-utils apache2 c9-core-installer bb-node-red-installer nodejs bb-beaglebone-io-installer bb-johnny-five-installer bone101
 ## do not upgrade, since it WILL upgrade the kernel which will not work inside qemu
 #apt-get upgrade --yes
-apt-get install --yes --no-install-recommends aptitude zsh git vim rsync python-cups python-pip cups-daemon cups cups-filters foomatic-db-compressed-ppds foomatic-db-engine python printer-driver-hpcups printer-driver-gutenprint htop tmux python3-cups python3-pip samba cups-bsd
+apt-get install --yes --no-install-recommends aptitude zsh git vim rsync python-cups python-pip cups-daemon cups cups-filters foomatic-db-compressed-ppds foomatic-db-engine python printer-driver-hpcups printer-driver-gutenprint htop tmux python3-cups python3-pip samba cups-bsd inotify-tools
 apt-get autoremove --yes
 pip install Adafruit_BBIO
 pip3 install Adafruit_BBIO
